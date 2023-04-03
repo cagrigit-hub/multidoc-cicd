b46c3c797a94466d8800545f13a4eef7aa54fa31
cagrikutayok <cagrikutayok@hotmail.com>
2023-04-03 23:42:14 +0300

    bugs

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/backend', (req, res) => {
-    res.send('Hello from backend!');
+    res.send('Hello from backend! Çağrı CD');
 });
 
 app.listen(8080, () => console.log('Listening on port 8080!'));
